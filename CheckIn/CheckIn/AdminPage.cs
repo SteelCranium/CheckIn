7dfe9c7957f82af3e0d5bfc49870adaac0ef0a81
Christian Gross <christian.gross1016@gmail.com>
2014-11-19 08:41:03 -0600

    Manifest Changes
    permissions have been updated

--- CheckIn/CheckIn/AdminPage.cs
+++ CheckIn/CheckIn/AdminPage.cs
@@ -16,11 +16,22 @@ namespace CheckIn
 	[Activity (Label = "AdminPage")]			
 	public class AdminPage : Activity
 	{
+		int count = 1;
+
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
 
-			// Create your application here
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Admin_Page);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button> (Resource.Id.myButton);
+
+			button.Click += delegate {
+				button.Text = string.Format ("{0} clicks yo!", count++);
+			};
 		}
 	}
 }
