a1a91fccf7dae93f111d78fd49b2d55b75a31942
Connercc5 <Connercc5@gmail.com>
2014-11-19 08:43:13 -0600

    no
    no

--- CheckIn/CheckIn/Resources/Resource.designer.cs
+++ CheckIn/CheckIn/Resources/Resource.designer.cs
@@ -60,8 +60,26 @@ namespace CheckIn
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050001
+			public const int editText1 = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int editText2 = 2131034114;
+			
+			// aapt resource value: 0x7f050004
+			public const int editText3 = 2131034116;
+			
+			// aapt resource value: 0x7f050005
+			public const int editText4 = 2131034117;
+			
+			// aapt resource value: 0x7f050003
+			public const int editText5 = 2131034115;
+			
 			// aapt resource value: 0x7f050000
 			public const int myButton = 2131034112;
+			
+			// aapt resource value: 0x7f050006
+			public const int textView1 = 2131034118;
 			
 			static Id()
 			{
@@ -83,7 +101,10 @@ namespace CheckIn
 			public const int Main = 2130903041;
 			
 			// aapt resource value: 0x7f030002
-			public const int Student_Page = 2130903042;
+			public const int SignIn = 2130903042;
+			
+			// aapt resource value: 0x7f030003
+			public const int Student_Page = 2130903043;
 			
 			static Layout()
 			{
