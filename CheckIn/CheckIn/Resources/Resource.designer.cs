8bb4272e5be4db422c68e39f6c76e8ba5e801b98
Youssef Mahmoud <youssefpower1997@gmail.com>
2014-11-25 08:39:56 -0600

    Splash screen
    ayy lmoa :alien: :alien:

--- CheckIn/CheckIn/Resources/Resource.designer.cs
+++ CheckIn/CheckIn/Resources/Resource.designer.cs
@@ -53,6 +53,9 @@ namespace CheckIn
 			// aapt resource value: 0x7f020002
 			public const int maxresdefault = 2130837506;
 			
+			// aapt resource value: 0x7f020003
+			public const int splash = 2130837507;
+			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -66,26 +69,26 @@ namespace CheckIn
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050003
-			public const int email = 2131034115;
+			// aapt resource value: 0x7f060003
+			public const int email = 2131099651;
 			
-			// aapt resource value: 0x7f050001
-			public const int imageView1 = 2131034113;
+			// aapt resource value: 0x7f060001
+			public const int imageView1 = 2131099649;
 			
-			// aapt resource value: 0x7f050000
-			public const int myButton = 2131034112;
+			// aapt resource value: 0x7f060000
+			public const int myButton = 2131099648;
 			
-			// aapt resource value: 0x7f050004
-			public const int password = 2131034116;
+			// aapt resource value: 0x7f060004
+			public const int password = 2131099652;
 			
-			// aapt resource value: 0x7f050005
-			public const int phone = 2131034117;
+			// aapt resource value: 0x7f060005
+			public const int phone = 2131099653;
 			
-			// aapt resource value: 0x7f050002
-			public const int relativeLayout1 = 2131034114;
+			// aapt resource value: 0x7f060002
+			public const int relativeLayout1 = 2131099650;
 			
-			// aapt resource value: 0x7f050006
-			public const int signin = 2131034118;
+			// aapt resource value: 0x7f060006
+			public const int signin = 2131099654;
 			
 			static Id()
 			{
@@ -137,6 +140,22 @@ namespace CheckIn
 			}
 			
 			private String()
+			{
+			}
+		}
+		
+		public partial class Style
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int Theme_Splash = 2131034112;
+			
+			static Style()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Style()
 			{
 			}
 		}
---
+++ CheckIn/CheckIn/SplashScreen.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace CheckIn
+{
+	using System.Threading;
+
+	using Android.App;
+	using Android.OS;
+
+	[Activity(Theme = "@style/Theme.Splash", MainLauncher = true, NoHistory = true)]	
+	public class SplashScreen : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+			Thread.Sleep(2000); // Simulate a long loading process on app startup.
+			StartActivity(typeof(SignIn));
+			// Create your application here
+		}
+	}
+}
+
