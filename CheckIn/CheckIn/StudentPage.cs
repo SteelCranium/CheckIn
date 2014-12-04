3ebc7e3c07ca9e4df56fbfba49d8bff78ff94ab4
Connercc5 <Connercc5@gmail.com>
2014-12-04 08:03:20 -0600

    Added finish() to StuPage

--- CheckIn/CheckIn/StudentPage.cs
+++ CheckIn/CheckIn/StudentPage.cs
@@ -30,7 +30,7 @@ namespace CheckIn
 			Button button = FindViewById<Button> (Resource.Id.myButton);
 
 			button.Click += delegate {
-				button.Text = string.Format ("{0} clicks!", count++);
+				Finish();
 			};
 		}
 	}
