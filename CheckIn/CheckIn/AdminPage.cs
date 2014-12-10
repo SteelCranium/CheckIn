2ecbadd8f61f71a8045c29740810152dd62053c2
Connercc5 <Connercc5@gmail.com>
2014-12-10 08:23:10 -0600

    Deleted dumb

--- CheckIn/CheckIn/AdminPage.cs
+++ CheckIn/CheckIn/AdminPage.cs
@@ -10,13 +10,12 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
-
 namespace CheckIn
 {
 	[Activity (Label = "AdminPage")]			
 	public class AdminPage : Activity
 	{
-		int count = 1;
+
 
 		protected override void OnCreate (Bundle bundle)
 		{
@@ -27,11 +26,9 @@ namespace CheckIn
 
 			// Get our button from the layout resource,
 			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
 
-			button.Click += delegate {
-				button.Text = string.Format ("{0} clicks yo!", count++);
-			};
+
+		
 		}
 	}
 }
