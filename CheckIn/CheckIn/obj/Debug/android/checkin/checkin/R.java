c0f5e50a2880db366203e5cbe4406407faece6d1
Connercc5 <Connercc5@gmail.com>
2014-12-10 08:07:33 -0600

    Revert "gg"
    This reverts commit 3efd90acd7ac233778925ed38f73e3d30cefd11b.

--- CheckIn/CheckIn/obj/Debug/android/checkin/checkin/R.java
+++ CheckIn/CheckIn/obj/Debug/android/checkin/checkin/R.java
@@ -11,9 +11,10 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int checkin_small=0x7f020000;
-        public static final int maxresdefault=0x7f020001;
-        public static final int splash=0x7f020002;
+        public static final int checkin_logo=0x7f020000;
+        public static final int checkin_small=0x7f020001;
+        public static final int maxresdefault=0x7f020002;
+        public static final int splash=0x7f020003;
     }
     public static final class id {
         public static final int email=0x7f060003;
